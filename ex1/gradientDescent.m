4430714290e59887c756d2219d783c00ee5c37e1
Yang Su <suyang1+services@gmail.com>
2011-10-23 23:33:22 -0400

    correct implementation of gradientDescent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,13 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-    theta = theta - alpha * 1 / m * sum((X * theta' - y) * X)
-
-
-
-
-
+    
+    temp = zeros(length(theta),1);
+    for i = 1:length(theta)
+       temp(i) = theta(i) - alpha * 1/m * sum((X * theta - y) .* X(:,i));
+    end
+    theta = temp;
     % ============================================================
 
     % Save the cost J in every iteration    
