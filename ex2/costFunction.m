f7cd7ec134dd604ad83d39cdf2042a5196ae3dc3
Yang Su <suyang1+services@gmail.com>
2011-10-29 22:34:11 -0400

    completed correct implementation of costFunction

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -20,12 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+h = sigmoid(X * theta);
+J = 1/m * sum(-y .* log(h) - (1 - y) .* log(1 - h));
+for i=1:m
+  grad(i) = 1/m * sum((h - y) * X(i));
+end
 
 % =============================================================
 
