b3e4343a54ced9a2baa1608d007ec52afe0ee773
Yang Su <suyang1+services@gmail.com>
2011-10-29 23:17:39 -0400

    completed implementation for regularized logical regression

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,10 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+h = sigmoid(X * theta);
+J = 1/m * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + lambda/(2*m) * sum(theta .* theta);
+grad(1) = 1/m * sum((h - y)' * X(:,1));
+for i=2:size(theta)
+  grad(i) = 1/m * sum((h - y)' * X(:,i) + lambda * theta(i));
+end
 
 % =============================================================
 
