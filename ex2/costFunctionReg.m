1f149245855d845d5084d1d7b0af87e2c637cc60
Yang Su <suyang1+services@gmail.com>
2011-11-04 17:29:02 -0400

    suppressed theta2 output in ex2 as well

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -18,7 +18,7 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 h = sigmoid(X * theta);
-theta2 = theta(2:end)
+theta2 = theta(2:end);
 
 J = 1/m * sum(-y .* log(h) - (1-y) .* log(1-h)) + lambda/(2*m) * sum(theta2.^2);
 
