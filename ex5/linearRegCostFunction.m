0382e811132873c1b8ca26b78fe3cac17a04fe6e
Yang Su <suyang1+services@gmail.com>
2011-11-18 16:01:21 -0500

    corrected cost function

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-J = 1/(2*m) * sum((X * theta - y) .^ 2) + lambda/(2*m) * sum(theta .^ 2);
+J = 1/(2*m) * sum((X * theta - y) .^ 2) + lambda/(2*m) * sum(theta(2:end) .^ 2);
 grad = 1/m * X' * (X * theta - y) + lambda/m * [0; theta(2:end)];
 
 % =========================================================================
