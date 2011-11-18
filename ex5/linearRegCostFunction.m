bad31a11f68f93f3fe32508f9e1a860441005e3e
Yang Su <suyang1+services@gmail.com>
2011-11-18 15:33:42 -0500

    fixed error in grad

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -20,7 +20,7 @@ grad = zeros(size(theta));
 %
 
 J = 1/(2*m) * sum((X * theta - y) .^ 2) + lambda/(2*m) * sum(theta .^ 2);
-grad = 1/m * X' * (X * theta - y) + lambda/m * [theta(2:end)];
+grad = 1/m * X' * (X * theta - y) + lambda/m * [0; theta(2:end)];
 
 % =========================================================================
 
