0ed62c746dad5563ce2a2e6aeaff6e10e068f37a
Yang Su <suyang1+services@gmail.com>
2011-12-02 18:16:39 -0500

    completed compute centroid

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -25,13 +25,10 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
+for ind=1:K
+  indices = find(idx(:) == ind);
+  centroids(ind,:) = mean(X(indices,:));
+end
 
 % =============================================================
 
