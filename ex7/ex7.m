fd616740b7ba281764f563d0c5fbc4e491cc5f21
Yang Su <suyang1+services@gmail.com>
2011-12-02 18:17:35 -0500

    added code to fix errors in the image loading part

--- ex7/ex7.m
+++ ex7/ex7.m
@@ -106,8 +106,8 @@ pause;
 fprintf('\nRunning K-Means clustering on pixels from an image.\n\n');
 
 %  Load an image of a bird
-A = double(imread('bird_small.png'));
-
+% A = double(imread('bird_small.png'));
+load('parrot.mat.txt');
 % If imread does not work for you, you can try instead
 %   load ('bird_small.mat');
 
